55ab1a776eaad102a66aba848d1887848e5c5ef7
MarkDestroyer <markkir123@gmail.com>
2021-07-16 15:59:14 +0300

    swift

--- lesson-3/client-server-1347/vk-api/Controllers/UserViewController.swift
+++ lesson-3/client-server-1347/vk-api/Controllers/UserViewController.swift
@@ -21,9 +21,10 @@ class UserProfileViewController: UIViewController {
         let url = URL(string: "https://sun9-73.userapi.com/impg/Bk3AB70wnbEkrRo6W5eFKlAM3aMJ9QdqFcJjng/JEa5xTjc3aI.jpg?size=1620x2160&quality=96&sign=73c52aa02b488f595af638c784a95b14&type=album")!
        
         
-        // Fetch Image Data
+        
         if let data = try? Data(contentsOf: url) {
-            // Create Image and Update Image View
+           
+            
             imageView.image = UIImage(data: data)
             
             userApi.getUserInfo {user in
